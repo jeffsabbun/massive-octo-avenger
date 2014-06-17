b569d6872cf2f57c7155fbb53cee0da99e884b9d
jeffsabbun <jeffsabbun@gmail.com>
2014-06-17 12:51:05 -0400

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://avengerit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'avengerit.herokuapp.com' }
 end
+

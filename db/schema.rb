da4148a9a91ebd81ff212b82ba3e709cc88892f2
jeffsabbun <jeffsabbun@gmail.com>
2014-07-01 15:35:08 -0400

    db/schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,9 @@ ActiveRecord::Schema.define(version: 20140629214715) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "favorites", force: true do |t|
     t.integer  "post_id"

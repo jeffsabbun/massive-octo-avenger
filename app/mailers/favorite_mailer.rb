d9ecc6f14167dd9f54be7894bf625fe8d29df645
jeffsabbun <jeffsabbun@gmail.com>
2014-07-02 23:08:23 -0400

    Implemented favoriting

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,17 @@
+class FavoriteMailer < ActionMailer::Base
+  default from: "jeffsabbun@gmail.com"
+
+  def new_comment(user, post, comment)
+    @user = user
+    @post = post
+    @comment = comment
+
+    # New Headers
+    headers["Message-ID"] = "<comments/#{@comment.id}@avengerit.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{@post.id}@avengerit.herokuapp.com>"
+    headers["References"] = "<post/#{@post.id}@avengerit.herokuapp.com>"
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+end

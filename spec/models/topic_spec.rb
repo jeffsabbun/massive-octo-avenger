e4a6e54112098520cf8726ad0429b3b288e78f71
jeffsabbun <jeffsabbun@gmail.com>
2014-07-17 02:05:45 -0400

    Worked again on private topics

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,32 @@
+describe Topic do
+   describe "scopes" do
+ 
+     before do 
+       @public_topic = Topic.create # default is public
+       @private_topic = Topic.create(public: false)
+     end
+ 
+     describe "public" do
+       it "returns a relation of all public topics" do
+         expect(Topic.public).to eq( [@public_topic] )
+       end
+     end
+ 
+     describe "private" do
+       it "returns a relation of all private topics" do
+         # Your code here
+       end
+     end
+ 
+     describe "visible_to(user)" do
+       it "returns all specs if the user is present" do
+         user = true # sneaky solution; we don't need a real user, just something truthy
+         # Your code here
+       end
+ 
+       it "returns only public specs if user is nil" do
+         # Your code here
+       end
+     end
+   end
+ end

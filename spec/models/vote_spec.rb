198ef65931d0c78f2263ff9a940485e3c768b0a8
jeffsabbun <jeffsabbun@gmail.com>
2014-08-05 15:51:57 -0400

    fixed up spec for intro to rspec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,7 +4,14 @@ describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @vote.validations). to eq(true)
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
+
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
+
+        bad_v = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
       end
     end
   end

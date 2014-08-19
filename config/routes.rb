216d7ca8cbf23c0829d2ad999795469b3e80326a
jeffsabbun <jeffsabbun@gmail.com>
2014-08-19 15:12:46 -0400

    worked on popular posts and added missing code

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Bloccit::Application.routes.draw do
 
 
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts'
+  end
+  resources :posts, only: [:index] do
+
       resources :comments, only: [:create, :destroy]
       get '/up-vote' => 'votes#up_vote', as: :up_vote
       get '/down-vote' => 'votes#down_vote', as: :down_vote
       resources :favorites, only: [:create, :destroy]
-    end
   end
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
